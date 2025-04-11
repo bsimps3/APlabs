e9ce7134fcba39e0b260ecaa6b03b614da26b0e5
Brian Simpson <bsimps3@gmail.com>
2025-04-10 21:43:40 -0500

    Update __init__.py

--- 14arrays1/__init__.py
+++ 14arrays1/__init__.py
@@ -12,9 +12,9 @@ def totalrunner_main_exists():
     
 @check50.check(totalrunner_main_exists)
 def totalrunner_main_output():
-    """TotalRunner.main() output"""
+    """TotalRunner.main() output matches """
     expected = "12301\n-44\n-11568\n32767\n510\n476\n497\n-35\n-947\n1089\n-99\n6011\n"
     actual = check50_java.run("TotalRunner").stdout()
-    help_msg = "your output did match expected. Make sure your input data is an exact match"
+    help_msg = "your output did not match expected. Make sure your input data is an exact match"
     if actual != expected:
         raise check50.Mismatch(expected, actual, help=help_msg)
