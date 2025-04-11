388b35a27bc01b91ac5e84b4913ba6b35964f638
Brian Simpson <bsimps3@gmail.com>
2025-04-10 22:01:59 -0500

    Update __init__.py

--- 14arrays1/__init__.py
+++ 14arrays1/__init__.py
@@ -18,3 +18,23 @@ def totalrunner_main_output():
     help_msg = "your output did not match expected. Make sure your input data is an exact match"
     if actual != expected:
         raise check50.Mismatch(expected, actual, help=help_msg)
+        
+#
+
+@check50.check()
+def sumlastrunner_compiles():
+    check50_java.compile("SumLastRunner.java")
+    
+@check50.check(sumlastrunner_compiles)
+def sumlastrunner_main_exists():
+    """SumLastRunner can run"""
+    check50_java.checks.is_application_class("SumLastRunner")
+    
+@check50.check(sumlastrunner_main_exists)
+def sumlastrunner_main_output():
+    """SumLastRunner.main() output matches expected """
+    expected = "40\n55\n230\n-1\n-1\n119\n-1\n45\n24\n-1\n356\n10011\n"
+    actual = check50_java.run("SumLastRunner").stdout()
+    help_msg = "Drats! Your output did not match expected. Make sure your input data is an exact match OR your code is working"
+    if actual != expected:
+        raise check50.Mismatch(expected, actual, help=help_msg)
